a55dc398ddbc3870b1229cb4069d1b5dc9ab0c9a
VuVanNghia <vuvannghia.work@gmail.com>
2026-02-15 19:05:05 +0700

    Comment out Upstash Qstash topic configuration in main.tf for future reference

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ resource "upstash_qstash_topic_v2" "events" {
   endpoints = [
     # npx @upstash/qstash-cli@latest dev
 
-    # "https://dev-countdown-queries-service.toeichust.me/api/event-listener",
-    "https://countdown-queries-service.toeichust.me/api/event-listener",
+    "https://dev-countdown-queries-service.toeichust.me/api/event-listener",
+    # "https://countdown-queries-service.toeichust.me/api/event-listener",
 
   ]
 }
